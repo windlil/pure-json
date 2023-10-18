438e8054a8e9f70e417c2589990076ace6469f25
windlil <windlil@outlook.com>
2023-10-18 17:23:11 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
 import { jsonToElement } from '@/utils/jsonToElement'
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue'
 
-onMounted(() => {
-  jsonToElement(
-{
+function addHandler() {
+  let elementMap = new Map()
+  const controls = document.querySelectorAll('#controls')
+  for (let i = 0; i < controls.length; i++) {
+  controls[i].addEventListener('click', () => {
+
+    const parent = controls[i].parentElement
+    if (controls[i].getAttribute('data-id') === 'un') {
+      const child = parent!.children
+      const backChild: any[] = []
+      elementMap.set(parent, backChild)
+      for (let i = child.length - 2; i  >= 2; i--) {
+        backChild.push(child[i])
+        child[i].classList.add('none')
+      }
+      controls[i].setAttribute('data-id', 'ep')
+      } else {
+        const child: any[] = elementMap.get(parent)
+        for (let i = 0; i < child.length; i++) {
+          child[i].classList.remove('none')
+        }
+        controls[i].setAttribute('data-id', 'un')
+      }
+  })
+}
+}
+
+const value = ref()
+
+function change() {
+  console.log(value.value)
+  jsonToElement({
     "string": "街角小林",
     "number": 123,
     "boolean": true,
@@ -13,6 +42,8 @@ onMounted(() => {
     "emptyArray": [],
     "emptyObject": {},
     "array": [
+        "街角小林",
+        123,
         true,
         null,
         "",
@@ -76,11 +107,18 @@ onMounted(() => {
         ]
     }
 }, '.container')
+}
+
+
+
+onMounted(() => {
+  addHandler()
 })
 </script>
 
 <template>
   <div class="app">
+    <input @change="change" v-model="value" />
     <div class="container">
      
     </div>
--- src/utils/jsonToElement.ts
+++ src/utils/jsonToElement.ts
@@ -1,5 +1,6 @@
 
 export function jsonToElement(json: any, el: string | HTMLElement | any) {
+
   el = typeof el === 'string' ? document.querySelector(el) : el
   if (!el) return
   let originData = initJson(json)
@@ -18,36 +19,48 @@ function chargeBreak(data: any) {
 function deep(data: any, container: HTMLElement) {
   for (const item in data) {
     const currentNode = document.createElement('div')
+
     currentNode.style.marginLeft = '20px'
     if (typeof data[item] === 'object' && data[item] !== null) {
-      currentNode.textContent = objectTextContent(data, item)
+      currentNode.innerHTML = objectTextContent(data, item)
       container.appendChild(currentNode)
       const isBreak = chargeBreak(data[item]) 
       if (isBreak) continue
+
       deep(data[item], currentNode)
-      const end = document.createElement('div')
-      end.textContent = `${Array.isArray(data[item]) ? '],' : '},'}`
-      container.appendChild(end)
+      const end = document.createElement('p')
+      end.innerHTML = `${Array.isArray(data[item]) ? '],' : '},'}`
+      currentNode.appendChild(end)
       end.style.marginLeft = '20px'
     } else {
-      currentNode.textContent = `"${item}" : "${data[item]}",`
+      const value = typeof data[item] === 'string' ? `"${data[item]}"` : data[item]
+      const color = setValueColor(data[item]) as string
+      currentNode.innerHTML = `<span class="key">"${item}"</span> : <span class="${color}">${value}</span>,`
       container.appendChild(currentNode)
     }
   } 
 }
 
+
+
 function objectTextContent(data: any, item: string) {
   const isBreak = chargeBreak(data[item])
-
   if (Array.isArray(data[item])) {
-    return `"${item}" [${data[item].length}] ${isBreak ? ',' : '['}`
+    return `<span class="key">"${item}"</span> : ${isBreak ? '[0],' :`<span data-id="un" id="controls">-</span>[`}`
   } else {
-    return `"${item}" {${Object.keys(data[item]).length}\} ${isBreak ? ',' : '{'}`
+    return `<span class="key">"${item}"</span> : ${isBreak ? '{0},' : `<span data-id="un" id="controls">-</span>{`}`
   }
 }
 
 function initJson(data: any): any {
   let originData = JSON.stringify(data)
-  originData = JSON.parse(originData)
-  return originData
+
+
+  return JSON.parse(originData)
+}
+
+function setValueColor(data: any) {
+  if (typeof data === 'string') return 'string'
+  else if (typeof data === 'boolean') return 'boolean'
+  else if (typeof data === 'number') return 'number'
 }
